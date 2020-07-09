0bac85947a9660c761841079e9dfbaefaf3bd61b
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-07-09 14:11:58 +0530

    GUI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 
-function App() {
-  const [inputFields, setInputFields] = useState([
-    {
-      firstName: '',
-      lastName: '',
-    },
-  ]);
+const App = () => {
+  const [users, setUsers] = useState([{ firstName: '', lastName: '' }]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(inputFields);
+  const addUser = () => {
+    const values = [...users, { firstName: '', lastName: '' }];
+    setUsers(values);
   };
 
-  const handleInputChange = (i, e) => {
-    const values = [...inputFields];
+  const removeUser = (i) => {
+    let values = [...users];
+
+    values = values.filter((value, index) => i !== index);
+    setUsers(values);
+  };
+
+  const onChange = (i, e) => {
+    const values = [...users];
 
     if (e.target.name === 'firstName') {
       values[i].firstName = e.target.value;
@@ -22,70 +24,58 @@ function App() {
       values[i].lastName = e.target.value;
     }
 
-    setInputFields(values);
+    setUsers(values);
   };
 
-  const addField = () => {
-    const values = [...inputFields];
-    values.push({ firstName: '', lastName: '' });
-    setInputFields(values);
-  };
-
-  const removeField = (i) => {
-    let values = [...inputFields];
-    if (values.length > 1) {
-      values = values.filter((value, index) => i !== index);
-      setInputFields(values);
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(users);
   };
 
   return (
-    <div className="container pt-5">
-      <h1 className="text-center mb-5">Dynamic Form Fields in React</h1>
+    <div className="container my-5">
+      <form className="w-50" onSubmit={onSubmit}>
+        {users.map((user, i) => (
+          <div className="card mb-3" key={i}>
+            <div className="card-body">
+              <button
+                className="btn btn-danger btn-sm float-right mb-3"
+                onClick={() => removeUser(i)}
+              >
+                x
+              </button>
 
-      <form onSubmit={handleSubmit}>
-        {inputFields.map((field, i) => (
-          <div className="row" key={i}>
-            <div className="form-group col-sm-5">
-              <input
-                type="text"
-                className="form-control"
-                name="firstName"
-                placeholder="First Name"
-                onChange={(e) => handleInputChange(i, e)}
-                value={field.firstName}
-              />
-            </div>
-            <div className="form-group col-sm-5">
-              <input
-                type="text"
-                className="form-control"
-                name="lastName"
-                placeholder="Last Name"
-                onChange={(e) => handleInputChange(i, e)}
-                value={field.lastName}
-              />
-            </div>
-            <div className="form-group col-sm-2">
-              <button
-                className="btn btn-primary btn-sm mr-2"
-                onClick={addField}
-              >
-                +
-              </button>
-              <button
-                className="btn btn-danger btn-sm"
-                onClick={() => removeField(i)}
-              >
-                -
-              </button>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="firstName"
+                  className="form-control"
+                  placeholder="First Name"
+                  value={user.firstName}
+                  onChange={(e) => onChange(i, e)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="lastName"
+                  className="form-control"
+                  placeholder="Last Name"
+                  value={user.lastName}
+                  onChange={(e) => onChange(i, e)}
+                />
+              </div>
             </div>
           </div>
         ))}
+
         <input type="submit" value="Save" className="btn btn-success" />
+        <button className="btn btn-primary ml-2" onClick={addUser}>
+          +
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default App;
